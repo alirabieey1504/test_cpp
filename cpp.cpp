defd18b0762c623d71a0da5e35f4de3bec5d4abc
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-18 16:17:25 +0330

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,289 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+#define getch()
+using namespace std;
+int main()
+{
+  // #1
+  //  int a;
+  //  cout << "ali" << '\a' << "eter ";
+  //  cin >> a;
+  //  cout << a * 3;
+
+  // #2
+  //    int a,b;
+  //    cout<<"enter a :";
+  //    cin>>a;
+  //    cout<<"enter b :";
+  //    cin>>b;
+  //    cout<<"hasel :"<<a+b;
+
+  // #3
+  //    double a;
+  //    a=3.333;
+  //    float b=3.4;
+  //    cout<<sizeof(a)<<endl;
+  //    cout<<sizeof(b)<<endl;
+
+  // #4
+
+  // char ch;
+  // ch='A';
+  // cout<<ch+1;
+
+  // #5
+
+  //    int a,b;
+  //    a=19;
+  //    b=5;
+  //    b=4;
+  //    cout<<++b*--a;
+
+  // int a=2;
+  // int b;
+  // b=++a;
+  // cout<<"a :"<<a;
+  // cout<<"\n";
+  // cout<<"b :"<<b;
+
+  // #7
+
+  // int a=2;
+  // int b;
+  // b= a * ++a;
+  // cout<<"b :"<<b;
+
+  // #8
+  // int a=2;
+  // int b=3;
+
+  //    cout<<(a<b);
+
+  // #9
+  //   bool a,b;
+  //   a=true;
+  //   b=false;
+  // //   cout<<(a&&b);
+  // //   cout<<(!b);
+  //   cout<<(a||b);
+
+  // # 10
+
+  // int a = 2;
+  // a = a + 3;
+  // a += 3;
+  // cout << a;
+
+  //  -=
+  //  +=
+  //  *=
+  //  /=
+  // int x=5;
+  // x*=3;
+  // cout<<x;
+
+  // int x,y,z;
+  // x=8;
+  // y=x<<1;
+  // cout<<"y :"<<y<<endl;
+  // z=x>>1;
+  // cout<<"z :"<<z;
+
+  //   int x=6;
+  //   int y=4;
+  //   int z=x|y;
+  //   cout<<z;
+  // int x=6;
+  //  int y=4;
+  // int z=x&&y;
+  // cout<<z;
+
+  //  int x=8;
+  //  char y;
+  //  y=x>13 ? 'A': 'B';
+  //  cout<<y;
+
+  // int x=8;
+  // int y=4;
+  // int z;
+  // z=x>y ? x:y;
+  // #10
+  //  int r;
+  //  float x;
+  //  cout << "enter r: ";
+  //  cin >> r;
+  //  x = 3.14 * r * r;
+  //  cout << "area= " << x;
+
+  // int x,y;
+  // float avl;
+  // cin>>x;
+  // cin>>y;
+
+  // avl=(float)(x+y)/2;
+  // cout<<"avl :"<<avl<<endl;
+
+  // #10
+  //  int x=5;
+  //  cout.width(3);
+  //  cout.fill('&');
+  //  cout<<x;
+  // #11
+
+  // cout<<pow(2,3)<<endl;
+  // cout<<pow(2,-2)<<endl;
+  // cout<<pow(4,5)<<endl;
+
+  // cout<<endl<<endl;
+  // cout<<fmod(49,7);
+  // cout<<abs(-2.3);
+  // cout<<fabs(-2.4);
+
+  // cout<<floor(22.4);
+  // cout<<ceil(32.56);
+  // cout<<log10(100)<<endl;
+  // cout<<log(3.4)
+
+  // cout<<tolower('A');
+  // cout<<islower('f');
+  // cout<<char(76);
+
+  // #12
+
+  // int x;
+  // cin>>x;
+  // if(x>=0)
+  //   cout<<"print ";
+  // else
+  // {
+  //   cout<<"negetive";
+  // }
+
+  //  //2
+  //  int a,b;
+  //  cin>>a>>b;
+  //  if(a%b==0)
+  //  cout<<"yes";
+  //  else
+  //  cout<<"no"
+
+  // int a,b;
+  // cout<<"enter :";
+  // cin>>a>>b;
+  // if(a<b)
+  // cout<<"min :"<<a;
+  // else
+  // cout<<"min="<<b;
+
+  // int n;
+  // cin>>n;
+  // if(n==13)
+  // cout<<"yes";
+  // else
+  // cout<<"no"
+
+  // #13
+
+  // int a, b, c;
+  // cout << "enter number :";
+  // cin >> a >> b >> c;
+  // int min;
+  // min = a;
+  // if (b < min)
+  //   min = b;
+  //   if(c<min)
+  //   min=c;
+
+  //   cout<<"min :"<<min
+
+  // int  a,b,c;
+  // cout<<"enter number :";
+  // cin>>a>>b;
+  // if(a>b){
+  // c=a;
+  // a=b;
+  // b=c;
+  // }
+  // cout<<a<<" "<<b;
+
+  // int a,b;
+  // cin>>a>>b;
+  // if (b!=0 && a%b==0)
+  // {
+  //   cout<<"yes";
+  // }
+  // else{
+  //   cout<<"no";
+  // }
+
+  // #15
+  // int a, b;
+  // cin >> a >> b;
+  // if (b != 0)
+  //   if (a % b == 0)
+  //     cout << "yes";
+  //   else
+  //     cout << "no";
+  // else
+  //   cout << "error";
+
+  // int a, b, c;
+  // cin >> a >> b >> c;
+  // cout << "enter numbet :";
+  // if (a < b)
+  //   if (a < c)
+  //     cout << a;
+  //   else
+  //     cout << c;
+  //     else
+  //     if(b<c)
+  //     cout<<b;
+  //     else
+  //     cout<<c;
+
+  // #16
+
+  // int g;
+  // cout << "greate ";
+  // cin >> g;
+  // if (g >= 17 && g <=20)
+  //   cout << 'a';
+  // else if (g >= 15 && g <17)
+  //   cout << 'b';
+  // else if (g >= 10 && g <15)
+  //   cout << 'c';
+  // else
+  //   cout << 'd';
+
+  // #17
+  // char ch;
+  // cout << "enter color :";
+  // cin >> ch;
+  // // if (ch == 'r')
+  // //   cout << "red";
+  // // else if (ch == 'g')
+  // //   cout << 'green';
+  // // else if (ch == 'f')
+  // //   cout << "foloa";
+  // // else
+  // //   cout << "nabod";
+
+  // switch (ch)
+  // {
+  // case 'r': cout << "red"; break;
+  // case 'b':cout << "blue";break;
+  // case 'g':cout<<"green";break;
+
+  // default:cout<<"error";
+  // }
+
+  // for (int i = 100; i >= 50; i -= 2)
+  //   cout << i << '\n';
+
+  // char ch;
+
+
+
+  getch();
+}
